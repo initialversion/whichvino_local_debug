ff275b6aeb03d689c9514772e392aef07cbd0060
Murugan <murugu2012@gmail.com>
2017-05-15 14:07:37 +0530

    Direct association between tasting_note_tags and wine_tag_pairs

--- app/models/tasting_note_tag.rb
+++ app/models/tasting_note_tag.rb
@@ -1,6 +1,10 @@
 class TastingNoteTag < ApplicationRecord
   # Direct associations
 
+  has_one    :wine_tag_pair,
+             :foreign_key => "tag_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/wine_tag_pair.rb
+++ app/models/wine_tag_pair.rb
@@ -1,6 +1,10 @@
 class WineTagPair < ApplicationRecord
   # Direct associations
 
+  belongs_to :wine,
+             :class_name => "TastingNoteTag",
+             :foreign_key => "tag_id"
+
   belongs_to :tag,
              :class_name => "Wine"
 
